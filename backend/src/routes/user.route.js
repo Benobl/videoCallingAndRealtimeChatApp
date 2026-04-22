6e43f6f83c7ac17a17ba02339e269ac4bd874371
Nebil <nebilbromance@gmail.com>
2026-04-22 13:44:10 +0300

    add sendingfriendequest,acceptfriends request

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import {getRecommendedUsers,getMyFriends} from "../controllers/user.controller.js"
+import { sendFriendRequest } from "../controllers/user.controller.js"
+import {getRecommendedUsers,getMyFriends, sendFriendRequest,acceptFriendRequest,getFriendRequest} from "../controllers/user.controller.js"
 const router=express.Router()
 //apply auth middleware to all routes
 router.use(protectRoute)
 router.get('/',getRecommendedUsers)
 router.get('/',getMyFriends)
-
+router.post("/friend-request/:id",sendFriendRequest)
+router.put("/friend-request/:id/accept",acceptFriendRequest)
+router.get("/friend-requests",getFriendRequest)
 export default router;
