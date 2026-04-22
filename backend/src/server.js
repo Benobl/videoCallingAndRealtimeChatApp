343f37032c89d58b0867275ec1a9103705302e58
Nebil <nebilbromance@gmail.com>
2026-04-22 19:12:46 +0300

    authenticates riuting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,26 @@
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
-import cookieParser from "cookie-parser"
 
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import userRoutes from "./routes/user.route.js";
-import chatRoutes from "./routes/chat.route.js"
 const app = express();
 const PORT = process.env.PORT;
 
-
-app.use(express.json())
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, //allow fronend to send cookies
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/chat",chatRoutes)
+app.use("/api/chat", chatRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);
   connectDB();
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function SignUpPage() {
+  const [SignUpData,setSignUpData]=useState({
+fullName:"",
+email:"",
+password:""
+  });
+  const handleSignUp=(e)=>{
+    e.preventDefault();
+  }
+  return (
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">
+      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden'>
+
+      </div>
+    </div>
+  )
+}
+
+export default SignUpPage
